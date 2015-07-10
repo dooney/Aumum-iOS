5bfdc426e66ac34aff4fabf732b4e24bd3eb8622
Simpson Du <isimpsondu@gmail.com>
2015-07-10 05:11:43 -0700

    Add EaseMob.

---
+++ iosapp/iosapp/Aspect-EaseMob.m
@@ -0,0 +1,227 @@
+//
+//  Aspect-EaseMob.m
+//  iosapp
+//
+//  Created by Simpson Du on 9/07/2015.
+//  Copyright (c) 2015 YU XING TECHNOLOGY PTY. LTD. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <XAspect/XAspect.h>
+
+#define AtAspect  EaseMob
+
+#define AtAspectOfClass AppDelegate
+@classPatchField(AppDelegate)
+AspectPatch(-, BOOL, application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions)
+{
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    NSString *appkey = [ud stringForKey:@"identifier_appkey"];
+    if (!appkey)
+    {
+        appkey = @"easemob-demo#chatdemoui";
+        [ud setObject:appkey forKey:@"identifier_appkey"];
+    }
+    NSNumber *specifyServer = [ud objectForKey:@"identifier_enable"];
+    if (!specifyServer)
+    {
+        specifyServer = [NSNumber numberWithBool:NO];
+        [ud setObject:specifyServer forKey:@"identifier_enable"];
+    }
+    NSString *imServer = [ud stringForKey:@"identifier_imserver"];
+    if (!imServer)
+    {
+        imServer = @"im1.easemob.com";
+        [ud setObject:imServer forKey:@"identifier_imserver"];
+    }
+    NSString *imPort = [ud stringForKey:@"identifier_import"];
+    if (!imPort)
+    {
+        imPort = @"443";
+        [ud setObject:imPort forKey:@"identifier_import"];
+    }
+    NSString *restServer = [ud stringForKey:@"identifier_restserver"];
+    if (!restServer)
+    {
+        restServer = @"a1.easemob.com";
+        [ud setObject:restServer forKey:@"identifier_restserver"];
+    }
+    [ud synchronize];
+    
+    [[EaseMob sharedInstance].chatManager setIsAutoFetchBuddyList:YES];
+    
+    // 注册环信监听
+    [self registerEaseMobNotification];
+    [[EaseMob sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    [self setupNotifiers];
+    
+    return XAMessageForward(application:application didFinishLaunchingWithOptions:launchOptions);
+}
+
+// 监听系统生命周期回调，以便将需要的事件传给SDK
+- (void)setupNotifiers{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidEnterBackgroundNotif:)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillEnterForeground:)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidFinishLaunching:)
+                                                 name:UIApplicationDidFinishLaunchingNotification
+                                               object:nil];
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidBecomeActiveNotif:)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillResignActiveNotif:)
+                                                 name:UIApplicationWillResignActiveNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appDidReceiveMemoryWarning:)
+                                                 name:UIApplicationDidReceiveMemoryWarningNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appWillTerminateNotif:)
+                                                 name:UIApplicationWillTerminateNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appProtectedDataWillBecomeUnavailableNotif:)
+                                                 name:UIApplicationProtectedDataWillBecomeUnavailable
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(appProtectedDataDidBecomeAvailableNotif:)
+                                                 name:UIApplicationProtectedDataDidBecomeAvailable
+                                               object:nil];
+}
+
+#pragma mark - notifiers
+- (void)appDidEnterBackgroundNotif:(NSNotification*)notif{
+    [[EaseMob sharedInstance] applicationDidEnterBackground:notif.object];
+}
+
+- (void)appWillEnterForeground:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationWillEnterForeground:notif.object];
+}
+
+- (void)appDidFinishLaunching:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationDidFinishLaunching:notif.object];
+}
+
+- (void)appDidBecomeActiveNotif:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationDidBecomeActive:notif.object];
+}
+
+- (void)appWillResignActiveNotif:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationWillResignActive:notif.object];
+}
+
+- (void)appDidReceiveMemoryWarning:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationDidReceiveMemoryWarning:notif.object];
+}
+
+- (void)appWillTerminateNotif:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationWillTerminate:notif.object];
+}
+
+- (void)appProtectedDataWillBecomeUnavailableNotif:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationProtectedDataWillBecomeUnavailable:notif.object];
+}
+
+- (void)appProtectedDataDidBecomeAvailableNotif:(NSNotification*)notif
+{
+    [[EaseMob sharedInstance] applicationProtectedDataDidBecomeAvailable:notif.object];
+}
+
+#pragma mark - registerEaseMobNotification
+- (void)registerEaseMobNotification{
+    [self unRegisterEaseMobNotification];
+    // 将self 添加到SDK回调中，以便本类可以收到SDK回调
+    [[EaseMob sharedInstance].chatManager addDelegate:self delegateQueue:nil];
+}
+
+- (void)unRegisterEaseMobNotification{
+    [[EaseMob sharedInstance].chatManager removeDelegate:self];
+}
+
+#pragma mark - IChatManagerDelegate
+// 开始自动登录回调
+-(void)willAutoLoginWithInfo:(NSDictionary *)loginInfo error:(EMError *)error
+{
+    UIAlertView *alertView = nil;
+    if (error) {
+        alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"prompt", @"Prompt") message:NSLocalizedString(@"login.errorAutoLogin", @"Automatic logon failure") delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", @"OK") otherButtonTitles:nil, nil];
+        
+        //发送自动登陆状态通知
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"loginStateChange" object:@NO];
+    }
+    else{
+        alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"prompt", @"Prompt") message:NSLocalizedString(@"login.beginAutoLogin", @"Start automatic login...") delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", @"OK") otherButtonTitles:nil, nil];
+        
+        //将旧版的coredata数据导入新的数据库
+        EMError *error = [[EaseMob sharedInstance].chatManager importDataToNewDatabase];
+        if (!error) {
+            error = [[EaseMob sharedInstance].chatManager loadDataFromDatabase];
+        }
+    }
+    
+    [alertView show];
+}
+
+// 结束自动登录回调
+-(void)didAutoLoginWithInfo:(NSDictionary *)loginInfo error:(EMError *)error
+{
+    UIAlertView *alertView = nil;
+    if (error) {
+        alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"prompt", @"Prompt") message:NSLocalizedString(@"login.errorAutoLogin", @"Automatic logon failure") delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", @"OK") otherButtonTitles:nil, nil];
+        
+        //发送自动登陆状态通知
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"loginStateChange" object:@NO];
+    }
+    else{
+        //获取群组列表
+        [[EaseMob sharedInstance].chatManager asyncFetchMyGroupsList];
+        
+        alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"prompt", @"Prompt") message:NSLocalizedString(@"login.endAutoLogin", @"End automatic login...") delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", @"OK") otherButtonTitles:nil, nil];
+    }
+    
+    [alertView show];
+}
+
+// 好友申请回调
+- (void)didReceiveBuddyRequest:(NSString *)username
+message:(NSString *)message
+{
+    if (!username) {
+        return;
+    }
+    if (!message) {
+        message = [NSString stringWithFormat:NSLocalizedString(@"friend.somebodyAddWithName", @"%@ add you as a friend"), username];
+    }
+}
+
+@end
+#undef AtAspectOfClass
+#undef AtAspect
